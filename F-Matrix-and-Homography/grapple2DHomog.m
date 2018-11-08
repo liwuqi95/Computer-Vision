7ca0d3bad1dbabda4950ad21134791e3c769d4cf
liwuqi95 <wuqi.li@mail.utoronto.ca>
2018-11-07 19:42:10 -0500

    finalize

--- F-Matrix-and-Homography/grapple2DHomog.m
+++ F-Matrix-and-Homography/grapple2DHomog.m
@@ -56,27 +56,28 @@ if size(imPts,1) == 2
   imPts = cat(1, imPts, ones(1,nPts,2));
 end
 
-%% RANSAC for F
+%% RANSAC for H
 seeds = {};
 sigma = 2.0; rho = 2;
 for kTrial = 1: nTrial
-  %% Test out F matrix on a random sample of 8 points
+  %% Test out H matrix on a random sample of 8 points
   idTest = randperm(nPts);
-  nTest = min(8, nPts);
+  nTest = min(4, nPts);
   idTest = idTest(1:nTest);
 
-  %% Solve for F matrix on the random sample
-  [F Sa Sf] = linEstF(imPts(:,idTest,1), imPts(:,idTest,2),1);
+  %% Solve for H matrix on the random sample
+  [H Sa] = linEstH(imPts(:,idTest,1), imPts(:,idTest,2),1);
   
-  %% Compute perpendicular error of all points to epipolar lines
-  perpErrL = zeros(1,nPts);
+  %% Compute distance error of all points to epipolar lines
+  DErr = zeros(1,nPts);
   for k = 1:nPts
-    lk = imPts(:,k,2)' * F';
-    perpErrL(k) = (lk* imPts(:,k,1))/norm(lk(1:2));
+    est = (H * imPts(:,k,1));
+    estP = est / est(3) - imPts(:,k,2);
+    DErr(k) = sqrt(estP(1)^2 + estP(2)^2);
   end
   
   %% Detect inliers
-  idInlier = abs(perpErrL) < rho*sigma;
+  idInlier = abs(DErr) < rho*sigma;
   
   %% Count inliers
   nInlier = sum(idInlier);
@@ -86,7 +87,7 @@ for kTrial = 1: nTrial
     seed.id = idTest;
     seed.idInlier = idInlier;
     seed.nInlier = nInlier;
-    seed.F = F;
+    seed.H = H;
     
     kSeed = length(seeds)+1;
     seeds{kSeed} = seed;
@@ -102,8 +103,8 @@ end
 [nM ks] = max(nInliers);
 nInliers(ks)
 
-%%  Refine estimate of F using all inliers.
-F = seeds{ks}.F;
+%%  Refine estimate of H using all inliers.
+H = seeds{ks}.H;
 idInlier = seeds{ks}.idInlier;
 
 idInlierOld = idInlier;
@@ -111,15 +112,19 @@ sum(idInlier)
 %% Do at most 10 iterations attempting to entrain as many points as possible.
 for kIt = 1: 10
   %% Fit F using all current inliers
-  [F Sa Sf] = linEstF(imPts(:,idInlier,1), imPts(:,idInlier,2),1);
+  [H Sa] = linEstH(imPts(:,idInlier,1), imPts(:,idInlier,2),1);
   
-  %% Compute perpendicular error to epipolar lines
-  perpErrL = zeros(1,nPts);
+  %% Compute distance error to epipolar lines
+  DErr = zeros(1,nPts);
   for k = 1:nPts
-    lk = imPts(:,k,2)' * F';
-    perpErrL(k) = (lk* imPts(:,k,1))/norm(lk(1:2));
+    est = (H * imPts(:,k,1));
+    estP = est / est(3) - imPts(:,k,2);
+    DErr(k) = sqrt(estP(1)^2 + estP(2)^2);
   end
-  idInlier = abs(perpErrL) < rho*sigma;
+  
+  %% Detect inliers
+  idInlier = abs(DErr) < rho*sigma;
+  
   nInlier = sum(idInlier)
   
   %% If we have the same set of inliers as the previous iteration then stop.
@@ -149,27 +154,25 @@ end
 resizeImageFig(hFig, size(imDwn), 1); hold on;
 set(get(hFig,'CurrentAxes'),'Ydir','reverse');
 hold on;
-% Plot all interest point locations as blue .'s
-plot(imPts(1,:,1), imPts(2,:,1), '.b');
-set(gca,'YDir', 'reverse');
-ax = axis;
-cropBox = [ax(1) ax(3) ax(2) ax(4)];
-title('Epipolar Lines in Left Image');
-for kl = 1:length(idLines)
-  % Plot interest point location corresponding to epipolar line as a "o"
-  % in the same colour as the epipolar line.
-  k = idLines(kl);
-  plot(imPts(1,k,1), imPts(2,k,1), 'o', 'Color', col(mod(k,nCol)+1,:));
-  % Plot epipolar line.
-  lk = imPts(:,k,2)' * F';
-  epk = cropLineInBox(lk(1:2), lk(3), cropBox); 
-  set(line(epk(:,1), epk(:,2)), 'Color', col(mod(k,nCol)+1,:));
+
+homogLeft = homogWarp(imLeft, inv(H));
+%% Show left Homog image
+SUPERIMPOSE = TRUE;
+hFig = figure(3);
+clf;
+if SUPERIMPOSE
+  image(homogLeft);
+  colormap(gray(256));
 end
+resizeImageFig(hFig, size(imDwn), 1); hold on;
+set(get(hFig,'CurrentAxes'),'Ydir','reverse');
+hold on;
+
 
 
 %% Show right image
 SUPERIMPOSE = TRUE;
-hFig = figure(3);
+hFig = figure(4);
 clf;
 if SUPERIMPOSE
   image(imRight);
@@ -178,53 +181,16 @@ end
 resizeImageFig(hFig, size(imDwn), 1); hold on;
 set(get(hFig,'CurrentAxes'),'Ydir','reverse');
 hold on;
-% Plot all interest point locations as blue .'s
-plot(imPts(1,:,2), imPts(2,:,2), '.b');
-set(gca,'YDir', 'reverse');
-ax = axis;
-cropBox = [ax(1) ax(3) ax(2) ax(4)];
-title('Epipolar Lines in Left Image');
-perpErrR = [];
-for kl = 1:length(idLines)
-  % Plot interest point location corresponding to epipolar line as a "o"
-  % in the same colour as the epipolar line.
-  k = idLines(kl);
-  plot(imPts(1,k,2), imPts(2,k,2), 'o', 'Color', col(mod(k,nCol)+1,:));
-  % Plot epipolar line.
-  lk = imPts(:,k,1)' * F;
-  epk = cropLineInBox(lk(1:2), lk(3), cropBox); 
-  set(line(epk(:,1), epk(:,2)), 'Color', col(mod(k,nCol)+1,:));
-  perpErrR = [perpErrR (lk * imPts(:,k,2)/norm(lk(1:2)))];
+
+homogRight = homogWarp(imRight, H);
+%% Show right Homog image
+SUPERIMPOSE = TRUE;
+hFig = figure(5);
+clf;
+if SUPERIMPOSE
+  image(homogRight);
+  colormap(gray(256));
 end
-
-%% Compute perpendicular distance to epipolar lines in left and right images.
-perpErrL = [];
-for k = 1:nPts
-  lk = imPts(:,k,2)' * F';
-  perpErrL = [perpErrL (lk * imPts(:,k,1))/norm(lk(1:2))];
-end
-perpErrR = [];
-for k = 1:nPts
-  lk = imPts(:,k,1)' * F;
-  perpErrR = [perpErrR (lk * imPts(:,k,2)/norm(lk(1:2)))];
-end
-
-%% Plot a histogram of the perpendicular distances
-err = [perpErrL perpErrR];
-err = min(err, 10);
-err = max(err, -10);
-[n b] = histo(err, 64);
-figure(4); clf;
-plot(b,n);
-title('Distance to epipolar line');
-xlabel('Error in pixels');
-ylabel('Frequency');
-
-%% Count inliers
-idL = abs(perpErrL)< rho*sigma;
-idR = abs(perpErrR) < rho*sigma;
-idInlier = idL & idR;
-sum(idInlier)
-sum(idInlier)/nPts
-
-%% save 'Fcorr' F Sa Sf idInlier nInliers
+resizeImageFig(hFig, size(imDwn), 1); hold on;
+set(get(hFig,'CurrentAxes'),'Ydir','reverse');
+hold on;
--- F-Matrix-and-Homography/utils/linEstH.m
+++ F-Matrix-and-Homography/utils/linEstH.m
@@ -1,41 +1,41 @@
 function [H, Sa] = linEstH(left, right, NUM_RESCALE)
-  % [H, Sa] = linEstH(left, right, NUM_RESCALE)
-  % Estimate the homography matrix H from two 3 x n matrices of
-  % corresponding points left and right.  
-  % Here left(:,k) x (H * right(:,k)) apprx= 0 where x denotes
-  % the 3D cross product.
-  % NUM_RESCALE (default TRUE) uses Hartley's rescaling. Always use
-  % rescaling, unless you wish to show how badly the un-normalized
-  % algorithm works.
-  % Returns H along with the singular values Sa of the 2nPts x 9 homogeneous
-  % linear system for H.
-    
-  if nargin < 3
+% [H, Sa] = linEstH(left, right, NUM_RESCALE)
+% Estimate the homography matrix H from two 3 x n matrices of
+% corresponding points left and right.
+% Here left(:,k) x (H * right(:,k)) apprx= 0 where x denotes
+% the 3D cross product.
+% NUM_RESCALE (default TRUE) uses Hartley's rescaling. Always use
+% rescaling, unless you wish to show how badly the un-normalized
+% algorithm works.
+% Returns H along with the singular values Sa of the 2nPts x 9 homogeneous
+% linear system for H.
+
+if nargin < 3
     NUM_RESCALE = 1;
-  end
-  
-  nPts = size(left,2);
-  if nPts < 4 | nPts ~= size(right,2)
+end
+
+nPts = size(left,2);
+if nPts < 4 | nPts ~= size(right,2)
     fprintf(2,'lineEstH: Innappropriate number of left and right points.');
     H = [];
     return;
-  end
-  
-  if size(left,1) == 2
+end
+
+if size(left,1) == 2
     left = [left; ones(1, nPts)];
-  else % Normalize to pixel coords
+else % Normalize to pixel coords
     left = left./repmat(left(3,:), 3,1);
-  end
-  if size(right,1) == 2
+end
+if size(right,1) == 2
     right = [right; ones(1, nPts)];
-  else % Normalize to pixel coords
+else % Normalize to pixel coords
     right = right./repmat(right(3,:), 3,1);
-  end
-  
-  imPts = cat(3, left, right);
-  
-  %% Rescale image data for numerical stability.
-  if NUM_RESCALE
+end
+
+imPts = cat(3, left, right);
+
+%% Rescale image data for numerical stability.
+if NUM_RESCALE
     Knum = repmat(eye(3), [1,1,2]);
     %%% Rescale for numerical stability
     mn = sum(imPts(1:2,:,:),2)/nPts;
@@ -49,25 +49,48 @@ function [H, Sa] = linEstH(left, right, NUM_RESCALE)
     Knum(1:2,3,:) = -mn;
     Knum(1:2,:,:) = Knum(1:2,:,:).*repmat(reshape(scl, [1 1 2]), [2, 3,1]);
     for kIm = 1:2
-      imPts(:,:,kIm) = reshape(Knum(:,:,kIm),3,3) * imPts(:,:,kIm);
+        imPts(:,:,kIm) = reshape(Knum(:,:,kIm),3,3) * imPts(:,:,kIm);
     end
     %% Sanity check
     % sum(imPts(1:2,:,:),2)/nPts  % Should be [0 0]'
     % sum(sum(imPts(1:2,:,:).^2,2)/nPts,1) % Should be 2.
-  end
+end
 
-  left
-  %% Make constraint matrix A. 
-  left = reshape(imPts(:,:,1), [3 nPts]);
-  right = reshape(imPts(:,:,2), [3 nPts]);
-  
-  
-  
-  
-  
-  left
-  
-  %% Bogus return values.  You need to fix this.
-  H = 0;
-  Sa = 0;
-  
+
+%% Make constraint matrix A.
+
+left = reshape(imPts(:,:,1), [3 nPts]);
+right = reshape(imPts(:,:,2), [3 nPts]);
+
+A = [];
+
+for i = 1 :size(left, 2)
+    
+    l_p = left(:, i);
+    r_p = right(:, i);
+    
+    A(end+1,:) = [0 0 0 -l_p(1) -l_p(2) -1 r_p(2)*l_p(1) r_p(2)*l_p(2) r_p(2)];
+    A(end+1,:) = [l_p(1) l_p(2) 1 0 0 0 -r_p(1)*l_p(1) -r_p(1)*l_p(2) -r_p(1)];
+    
+end
+
+% similar code from linEstF
+[Ua Sa Va] = svd(A); Sa = diag(Sa);
+H = reshape(Va(:,end), 3,3)';
+
+if NUM_RESCALE
+    H = reshape(Knum(:,:,2),3,3)^-1 * H * reshape(Knum(:,:,1),3,3);
+end
+
+H = H / norm(H);
+
+end
+
+
+
+
+
+
+
+
+
